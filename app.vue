f375dedcc5b22ae151b733ea17a2d694eae77588
Bernardo Oliveira <bernardo.oliveira@bovo.studio>
2023-01-25 09:21:20 +0000

    Initial commit

---
+++ app.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <Image image="/assets/images/mountain.jpg" />
+  </div>
+</template>
---
+++ components/image.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <img :src="props.image" />
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  image: {
+    type: String,
+    required: true,
+  },
+});
+</script>
